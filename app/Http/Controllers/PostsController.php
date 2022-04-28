20d46746ee56d379d6384c1971d688c3fc0b6ba9
khaledmo77 <khaled_mohamed9876@yahoo.com>
2022-04-28 03:29:01 +0200

    initial commit

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+use Intervention\Image\Facades\Image;
+
+class PostsController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index() {
+
+        $users = auth()->user()->following()->pluck('profiles.user_id');
+        $posts = Post::whereIn('user_id',$users)->with('user')->latest()->get();
+
+        return view('posts.index', compact('posts'));
+    }
+
+    public function create(){
+
+        return view('posts.create');
+    }
+
+    public function store() {
+
+        $data = request()->validate([
+            'caption' => 'required',
+            'image' => 'required|file|mimes:jpeg,jpg,png'
+        ]);
+
+        $imagePath = request('image')->store('uploads','public');
+
+        $image = Image::make(public_path("storage/{$imagePath}"))->fit(1200,1200);
+        $image->save();
+
+        auth()->User()->posts()->create([
+            'caption' => $data['caption'],
+            'image' => $imagePath,
+            // 'image' => 'required|file|mimes:jpeg,jpg,png'
+        ]);
+
+        return redirect('/profile/'.auth()->user()->id);
+    }
+
+    public function show(Post $post) {
+        
+        return view('posts.show', compact('post'));
+    }
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+## Monstergram
+
+Simple clone version of Instagram with Laravel
+- Register
+- Login
+- Follow / Unfollow user
+- Add new post
+- Change profile picture
+- View Single posts
+- Upload Image
+
+Youtube Link : https://youtu.be/-jO0WC86K5U
+
